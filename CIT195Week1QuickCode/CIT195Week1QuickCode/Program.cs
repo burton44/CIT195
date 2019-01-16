c558ed8b59046a39eae429fb39e80123458431b8
Richard Burton <44791210+burton44@users.noreply.github.com>
2019-01-16 15:21:33 -0500

    Add files via upload

---
+++ CIT195Week1QuickCode/CIT195Week1QuickCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIT195Week1QuickCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine();
+            Console.WriteLine("Press the Enter key to exit");
+            Console.ReadLine();
+        }
+    }
+}
