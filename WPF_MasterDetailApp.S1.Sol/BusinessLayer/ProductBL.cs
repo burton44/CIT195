a1706fd9566ec2071c915304f336f0fcc61a877e
Richard Burton <44791210+burton44@users.noreply.github.com>
2019-02-20 17:10:04 -0500

    Add files via upload

---
+++ WPF_MasterDetailApp.S1.Sol/BusinessLayer/ProductBL.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_MasterDetailApp;
+using WPF_MasterDetailApp.Models;
+
+namespace WPF_MasterDetailApp.BusinessLayer
+{
+    public class ProductBL
+    {
+        #region ENUMS
+
+
+
+        #endregion
+
+        #region FIELDS
+
+        ProductWindowView _productWindowView;
+        ProductWindowPresenter _productWindowPresenter;
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public ProductBL()
+        {
+            //
+            // instantiate the view model and initialize the data set
+            //
+
+            _productWindowPresenter = new ProductWindowPresenter(GetCompanyData(), GetProductData());
+
+            //
+            // instantiate, set the data context, and show the Main Window
+            //
+            _productWindowView = new ProductWindowView(_productWindowPresenter);
+            _productWindowView.DataContext = _productWindowPresenter;
+            _productWindowView.Show();
+        }
+
+        #endregion
+
+        #region METHODS
+
+        private Company GetCompanyData()
+        {
+            return new Company()
+            {
+                Installment = "The 1st James Bond Movie",
+            };
+        }
+
+        private Product GetProductData()
+        {
+            return
+                new Product()
+                {
+                    Id = 1,
+                    MovieTitle = "Dr. No",
+                    Director = "Terence Young",
+                    Actor = "Sean Connery",
+                    Villain = "Dr. No",
+                    ImageFileName = "drNo.jpg",
+                    Description = "James Bond's investigation of a missing colleague in Jamaica leads him to the island of the mysterious Dr. No and a scheme to end the US space program.",
+                    ReleaseDate = DateTime.Parse("10-05-1962"),
+                    BoxOfficeGross = 59500000
+                };
+        }
+
+        #endregion
+
+        #region EVENTS
+
+
+        #endregion
+
+    }
+}
---
+++ WPF_MasterDetailApp.S1.Sol/Models/Product.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MasterDetailApp.Models
+{
+    public class Product : ObservableObject
+    {
+        #region ENUMS
+
+        public enum GenderType { none, male, female }
+
+        #endregion
+
+        #region FIELDS
+
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private string _actor;
+        private string _villain;
+        private string _imageFileName;
+        private string _description;
+        private DateTime _hireDate;
+        private double _averageAnnualGross;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string MovieTitle
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                RaisePropertyChangedEvent("FullName"); // update items bound to the FullName property
+            }
+        }
+
+        public string Director
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                RaisePropertyChangedEvent("FullName"); // update items bound to the FullName property
+            }
+        }
+
+        public string Actor
+        {
+            get { return _actor; }
+            set { _actor = value; }
+        }
+        
+        public string Villain
+        {
+            get { return _villain; }
+            set { _villain = value; }
+        }
+
+        public string ImageFileName
+        {
+            get { return _imageFileName; }
+            set { _imageFileName = value; }
+        }
+        
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public DateTime ReleaseDate
+        {
+            get { return _hireDate; }
+            set { _hireDate = value; }
+        }
+
+        public double BoxOfficeGross
+        {
+            get { return _averageAnnualGross; }
+            set { _averageAnnualGross = value; }
+        }
+
+        public string FullName
+        {
+            get { return _firstName + " " + _lastName; }
+        }
+
+        public string ImageFilePath
+        {
+            get { return @"../Images/" + _imageFileName; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Product()
+        {
+
+        }
+
+        #endregion
+
+        #region METHODS
+
+
+        #endregion
+
+        #region EVENTS
+
+
+
+        #endregion
+    }
+}
