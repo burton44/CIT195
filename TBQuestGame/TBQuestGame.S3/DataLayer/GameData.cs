3c6ce7df73a09c711eeb712fec51b65105a12f61
Richard Burton <44791210+burton44@users.noreply.github.com>
2019-04-14 19:49:02 -0400

    Add files via upload

---
+++ TBQuestGame/TBQuestGame.S3/DataLayer/GameData.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTheAionProject.Models;
+
+namespace WpfTheAionProject.DataLayer
+{
+    /// <summary>
+    /// static class to store the game data set
+    /// </summary>
+    public static class GameData
+    {
+        public static Player PlayerData()
+        {
+            return new Player()
+            {
+                Id = 1,
+                Name = "Cpt. Rowsdower",
+                Age = 43,
+                JobTitle = Player.JobTitleName.Explorer,
+                Sex = Character.SexType.Male,
+                Health = 100,
+                Lives = 3,
+                ExperiencePoints = 0,
+                LocationId = 0,
+                Inventory = new ObservableCollection<GameItemQuantity>()
+                {
+
+                }
+            };
+        }
+
+        public static List<string> InitialMessages()
+        {
+            return new List<string>()
+            {
+                "\tYou are a pilot who has been awakened from cryo-sleep after 200 years aboard the deep space vessel Cryonaught. Upon resuscitation you realized the ship is in a mostly destroyed shamble, and you appear to be the lone survivor. ",
+                "\tChoose from the available nearby rooms where you would like to go or interact with the items in your current location."
+            };
+        }
+        private static GameItem GameItemById(int id)
+        {
+            return StandardGameItems().FirstOrDefault(i => i.Id == id);
+        }
+
+        public static GameMapCoordinates InitialGameMapLocation()
+        {
+            return new GameMapCoordinates() { Row = 0, Column = 0 };
+        }
+
+        public static Map GameMap()
+        {
+            int rows = 3;
+            int columns = 4;
+
+            Map gameMap = new Map(rows, columns);
+
+            //
+            // row 1
+            //
+            gameMap.MapLocations[0, 0] = new Location()
+            {
+                Id = 4,
+                Name = "Cryo-sleep Chamber 3B",
+                Description = "You awaken to find catastrophe, all of the other pods in your section appear to be " +
+                "cracked and leaking. The smell from the effluence spilling out of nearby pods is almost unbearable, " +
+                "the acrid stench of decay hanging in the air. The cabin is washed in a red spectrum of light, " +
+                "emitted by the emergency luminescence system. As you get your bearings you look around at the other " +
+                "pods to see that they are filled with the mangled and half-eaten corpses of your fellow pilots. "+
+                "One of the pods appears to have been broken from the inside out and no signs of a body remain. " +
+                "You see that ahead the cabin door has malfunction but is half way open. ",
+                Accessible = true,
+                ModifiyExperiencePoints = 10,
+                Message = "\tYou are a pilot who has been awakened from cryo-sleep after 200 years aboard the deep space vessel Cryonaught. Upon resuscitation you realized the ship is in a mostly destroyed shamble, and you appear to be the lone survivor. " +
+                "Choose from the available nearby rooms where you would like to go or interact with the items in your current location."
+            };
+            gameMap.MapLocations[0, 1] = new Location()
+            {
+                Id = 1,
+                Name = "Cryo-chamber Hallway",
+                Description = "The hallway is dimly lit, you see a sign above the South door that says 'Mess Hall' " +
+                "and a sign above the West door saying 'Cryo-sleep Chamber 3B', you look to see if any of the " +
+                "other chambers are still active but they have all been locked and welded shut. Above the other doors " +
+                "are signs that read 'Quarantined'.",
+                Accessible = true,
+                ModifiyExperiencePoints = 10,
+                Message = "Remember cadet, pick up any items you may see, they could come in handy later!",
+                  GameItems = new ObservableCollection<GameItemQuantity>()
+                {
+                    new GameItemQuantity(GameItemById(1001), 1)
+                }
+            };
+
+            //
+            // row 2
+            //
+            gameMap.MapLocations[1, 1] = new Location()
+            {
+                Id = 2,
+                Name = "Mess Hall",
+                Description = 
+                "Today the Mess Hall is well named because it is indeed a mess. A mess of scattered tables, chairs " +
+                "and shredded carcasses. You make your wake carefully through the rubble, electric wires hang down," +
+                "sending sparks crackling at random. The smell of ozone and loosed bowels fills your nostrils. To the " +
+                "North are the cryo chambers and to the South you can see the Barracks. To the East is a small bathroom," +
+                " a terrible smell eminates from within.",
+                Accessible = true,
+                ModifiyExperiencePoints = 10,
+                Message = "You have arrived at the mess hall. The heart of the Cryonaught deep space shuttle."
+            };
+            gameMap.MapLocations[1, 2] = new Location()
+            {
+                Id = 2,
+                Name = "Bathroom",
+                Description = 
+                "You are eaten by slime.",
+                Accessible = false,
+                ModifiyExperiencePoints = 50,
+                ModifyLives = -1,
+                RequiredExperiencePoints = 40
+            };
+
+            //
+            // row 3
+            //
+            gameMap.MapLocations[2, 0] = new Location()
+            {
+                Id = 3,
+                Name = "The Bridge",
+                Description = 
+                "The Bridge",
+                Accessible = false,
+                ModifiyExperiencePoints = 20,
+                ModifyHealth = 50,
+            };
+            gameMap.MapLocations[2, 1] = new Location()
+            {
+                Id = 4,
+                Name = "The Barracks",
+                Description = 
+                "The bunk house.",
+                Accessible = true,
+                ModifiyExperiencePoints = 10
+            };
+
+            return gameMap;
+        }
+        public static List<GameItem> StandardGameItems()
+        {
+            return new List<GameItem>()
+            {
+                new Items(1001, "Yellow Key Card", 75, "This will open the yellow door.", 10, "You used the yellow key card!", Items.UseActionType.OPENLOCATION),
+            };
+        }
+    }
+}
---
+++ TBQuestGame/TBQuestGame.S3/Models/Player.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+
+namespace WpfTheAionProject.Models
+{
+    /// <summary>
+    /// player class
+    /// </summary>
+    public class Player : Character
+    {
+        #region ENUMS
+
+        public enum JobTitleName { Explorer, MissionLeader, Supervisor }
+
+        #endregion
+
+        #region FIELDS
+
+        private int _lives;
+        private int _health;
+        private int _experiencePoints;
+        private int _wealth;
+        private JobTitleName _jobTitle;
+        private List<Location> _locationsVisited;
+        private ObservableCollection<GameItemQuantity> _inventory;
+        private ObservableCollection<GameItemQuantity> _items;
+     
+        #endregion
+
+        #region PROPERTIES
+
+        public int Lives
+        {
+            get { return _lives; }
+            set
+            {
+                _lives = value;
+                OnPropertyChanged(nameof(Lives));
+            }
+        }
+
+        public JobTitleName JobTitle
+        {
+            get { return _jobTitle; }
+            set
+            {
+                _jobTitle = value;
+                OnPropertyChanged(nameof(JobTitle));
+            }
+        }
+
+        public int ExperiencePoints
+        {
+            get { return _experiencePoints; }
+            set
+            {
+                _experiencePoints = value;
+                OnPropertyChanged(nameof(ExperiencePoints));
+            }
+        }
+        public int Health
+        {
+            get { return _health; }
+            set
+            {
+                _health = value;
+
+                if (_health > 100)
+                {
+                    _health = 100;
+                }
+                else if (_health <= 0)
+                {
+                    _health = 100;
+                    _lives--;
+                }
+
+                OnPropertyChanged(nameof(Health));
+            }
+        }
+
+        public int Wealth
+        {
+            get { return _wealth; }
+            set
+            {
+                _wealth = value;
+                OnPropertyChanged(nameof(Wealth));
+            }
+        }
+        public List<Location> LocationsVisited
+        {
+            get { return _locationsVisited; }
+            set { _locationsVisited = value; }
+        }
+        public ObservableCollection<GameItemQuantity> Inventory
+        {
+            get { return _inventory; }
+            set { _inventory = value; }
+        }
+
+        public ObservableCollection<GameItemQuantity> Items
+        {
+            get { return _items; }
+            set { _items = value; }
+        }
+
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Player()
+        {
+            _locationsVisited = new List<Location>();
+            _items = new ObservableCollection<GameItemQuantity>();
+        }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        /// update the game item category lists
+        /// </summary>
+        public void CalculateWealth()
+        {
+            Wealth = _inventory.Sum(i => i.GameItem.Value * i.Quantity);
+        }
+
+        public void UpdateInventoryCategories()
+        {
+            Items.Clear();
+
+
+            foreach (var gameItemQuantity in _inventory)
+            {
+                if (gameItemQuantity.GameItem is Items) Items.Add(gameItemQuantity);
+
+            }
+        }
+
+        /// <summary>
+        /// add selected item to inventory or update quantity if already in inventory
+        /// </summary>
+        /// <param name="selectedGameItemQuantity">selected item</param>
+        public void AddGameItemQuantityToInventory(GameItemQuantity selectedGameItemQuantity)
+        {
+            //
+            // locate selected item in inventory
+            //
+            GameItemQuantity gameItemQuantity = _inventory.FirstOrDefault(i => i.GameItem.Id == selectedGameItemQuantity.GameItem.Id);
+
+            if (gameItemQuantity == null)
+            {
+                GameItemQuantity newGameItemQuantity = new GameItemQuantity();
+                newGameItemQuantity.GameItem = selectedGameItemQuantity.GameItem;
+                newGameItemQuantity.Quantity = 1;
+
+                _inventory.Add(newGameItemQuantity);
+            }
+            else
+            {
+                gameItemQuantity.Quantity++;
+            }
+
+            UpdateInventoryCategories();
+        }
+
+        /// <summary>
+        /// remove selected item from inventory
+        /// </summary>
+        /// <param name="selectedGameItemQuantity">selected item</param>
+        public void RemoveGameItemQuantityFromInventory(GameItemQuantity selectedGameItemQuantity)
+        {
+            //
+            // locate selected item in inventory
+            //
+            GameItemQuantity gameItemQuantity = _inventory.FirstOrDefault(i => i.GameItem.Id == selectedGameItemQuantity.GameItem.Id);
+
+            if (gameItemQuantity != null)
+            {
+                if (selectedGameItemQuantity.Quantity == 1)
+                {
+                    _inventory.Remove(gameItemQuantity);
+                }
+                else
+                {
+                    gameItemQuantity.Quantity--;
+                }
+            }
+
+            UpdateInventoryCategories();
+        }
+
+
+        public bool HasVisited(Location location)
+        {
+            return _locationsVisited.Contains(location);
+        }
+
+        /// <summary>
+        /// override the default greeting in the Character class to include the job title
+        /// set the proper article based on the job title
+        /// </summary>
+        /// <returns>default greeting</returns>
+        public override string DefaultGreeting()
+        {
+            string article = "a";
+
+            List<string> vowels = new List<string>() { "A", "E", "I", "O", "U" };
+
+            if (vowels.Contains(_jobTitle.ToString().Substring(0, 1)));
+            {
+                article = "an";
+            }
+
+            return $"Hello, my name is {_name} and I am {article} {_jobTitle} for the Cryonaught space program.";
+        }
+
+        #endregion
+
+        #region EVENTS
+
+
+
+        #endregion
+
+    }
+}
